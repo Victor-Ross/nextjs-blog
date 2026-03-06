30a0b26ccbdd9fe7c7a73b533c45b254449fc133
Victor Ross <victor2ross@gmail.com>
2026-03-06 00:11:05 -0300

    DONE - loading spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function HomePage() {
-  return <h1>Hello world!</h1>;
+import { Suspense } from "react";
+
+import { PostsList } from "@/components/postsList";
+import { SpinLoader } from "@/components/spinLoader";
+
+export default async function HomePage() {
+  return (
+    <div>
+      <header>
+        <h1 className="text-6xl font-bold text-center py-8">HEADERRRR</h1>
+      </header>
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+      <footer>
+        <p className="text-6xl font-bold text-center py-8">footerRRR</p>
+      </footer>
+    </div>
+  );
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export function Header() {
+  return <h1 onClick={() => alert("123")}>Texto no meu h1</h1>;
+}
